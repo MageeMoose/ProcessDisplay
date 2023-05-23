86d6071111560d126f9e799ff284626e0a360190
MageeMoose <mageemoose@outlook.com>
2023-05-23 15:39:43 +0200

    Added Dialog pop-up

--- src/app/components/add-detainee-dialog/add-detainee-dialog.component.ts
+++ src/app/components/add-detainee-dialog/add-detainee-dialog.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, Inject} from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DetaineeModel } from 'src/app/models/detaineeModel';
+import { PounchdbService } from 'src/app/services/pounchdb.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-add-detainee-dialog',
@@ -7,6 +11,35 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./add-detainee-dialog.component.css']
 })
 export class AddDetaineeDialogComponent {
- // addDetainee: FormGroup
+  detaineeForm: FormGroup
 
+  constructor(public dialogRef: MatDialogRef<AddDetaineeDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DetaineeModel,
+    private formBuilder: FormBuilder,
+    private pounchService : PounchdbService) {
+      this.detaineeForm = this.formBuilder.group({
+        _id: [this.data._id],
+        _rev: [this.data._rev],
+        roomNumber: [this.data.roomNumber],
+        backgroundColor: [this.data.backgroundColor],
+        department: [this.data.department],
+        language: [this.data.language],
+        travel: [this.data.travel],
+        section: [this.data.section],
+        identification: [this.data.identification],
+        notes: [this.data.notes],
+        text: [this.data.text],
+        isSecMes: [this.data.isSecMes],
+        isVacant: [this.data.isVacant],
+        //hasVisitor: [this.data.model.hasVisitor]
+     });
+    }
+
+  onNoClick(): void {
+    this.pounchService.updateDetainee(this.data).then(() => {
+      this.dialogRef.close(this.data);
+    });
+  }
 }
+
+
